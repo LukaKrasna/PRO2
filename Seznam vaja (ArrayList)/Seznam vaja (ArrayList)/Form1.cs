8e5cfdfa6d8c0483d5568b5faa81ef30e5c4bba4
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-10-27 09:55:51 +0200

    Add files via upload

---
+++ Seznam vaja (ArrayList)/Seznam vaja (ArrayList)/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seznam_vaja__ArrayList_
+{
+    public partial class Form1 : Form
+    {
+        //Naloga: https://ucilnice.scng.si/pluginfile.php/4433/mod_resource/content/1/vaje1/vaja1pdf.pdf
+        private ArrayList seznam = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vnos = txtVnos.Text;
+            seznam.Add(vnos);
+            txtVnos.Text += "Dodan element " + vnos + " na konec seznama\n";
+            //txtVnos.Clear();
+        }
+
+        private void txtKonzola_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string vnos = txtVnos.Text;
+            if (seznam.Contains(vnos))
+            {
+                seznam.Remove(vnos);
+                txtKonzola.AppendText($"Odstranjen element '{vnos}'; ");
+            }
+            else
+            {
+                txtKonzola.AppendText($"Element '{vnos}' ni bil najden v seznamu; ");
+            }
+            txtKonzola.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count > 0)
+            {
+                string prviElement = seznam[0].ToString();
+                txtKonzola.AppendText($"Prvi element v seznamu: {prviElement}; ");
+            }
+            else
+            {
+                txtKonzola.AppendText("Seznam je prazen; ");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count > 0)
+            {
+                string zadnjiElement = seznam[seznam.Count - 1].ToString();
+                txtKonzola.AppendText($"Zadnji element v seznamu: {zadnjiElement}; ");
+            }
+            else
+            {
+                txtKonzola.AppendText("Seznam je prazen; ");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count == 0)
+            {
+                txtKonzola.AppendText("Seznam je prazen");
+            }
+            else
+            {
+                txtKonzola.AppendText("Seznam ni prazen");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string vnos = txtVnos.Text;
+            if (seznam.Contains(vnos))
+            {
+                txtKonzola.Text = ("Seznam vsebuje element " + vnos);
+            }
+            else
+            {
+                txtKonzola.Text = ("Seznam ne vsebuje elementa " + vnos);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string vnos = txtVnos.Text;
+            int lokacija = seznam.IndexOf(vnos);
+            if (lokacija != -1)
+            {
+                txtKonzola.AppendText("Lokacija elementa " + vnos + " v seznamu: " + lokacija);
+            }
+            else
+            {
+                txtKonzola.AppendText("Element " + vnos + " ni bil najden v seznamu");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            seznam.TrimToSize();
+            txtKonzola.AppendText("Seznam je skrajšan; ");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int velikost = seznam.Count;
+            txtKonzola.AppendText("Velikost seznama: " + velikost);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtKonzola.AppendText("Seznam:\n");
+            foreach (object element in seznam)
+            {
+                txtKonzola.AppendText(element.ToString() + "; ");
+            }
+        }
+
+        /*
+    {
+        private ArrayList seznam = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            string vnos = txtKonzola.Text;
+            seznam.Add(vnos);
+            txtKonzola.AppendText($"Dodan element '{vnos}' na konec seznama\n");
+            txtKonzola.Clear();
+        }
+
+        private void btnOdstrani_Click(object sender, EventArgs e)
+        {
+            string vnos = txtKonzola.Text;
+            if (seznam.Contains(vnos))
+            {
+                seznam.Remove(vnos);
+                txtKonzola1.AppendText($"Odstranjen element '{vnos}'\n");
+            }
+            else
+            {
+                txtKonzola1.AppendText($"Element '{vnos}' ni bil najden v seznamu\n");
+            }
+            txtKonzola1.Clear();
+        }
+
+        private void btnPrvi_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count > 0)
+            {
+                string prviElement = seznam[0].ToString();
+                txtKonzola1.AppendText($"Prvi element v seznamu: {prviElement}\n");
+            }
+            else
+            {
+                txtKonzola1.AppendText("Seznam je prazen\n");
+            }
+        }
+
+        private void btnZadnji_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count > 0)
+            {
+                string zadnjiElement = seznam[seznam.Count - 1].ToString();
+                txtKonzola1.AppendText($"Zadnji element v seznamu: {zadnjiElement}\n");
+            }
+            else
+            {
+                txtKonzola1.AppendText("Seznam je prazen\n");
+            }
+        }
+
+        private void btnJePrazna_Click(object sender, EventArgs e)
+        {
+            if (seznam.Count == 0)
+            {
+                txtKonzola1.AppendText("Seznam je prazen");
+            }
+            else
+            {
+                txtKonzola1.AppendText("Seznam ni prazen");
+            }
+        }
+
+        private void btnVsebuje_Click(object sender, EventArgs e)
+        {
+            string vnos = txtKonzola.Text;
+            if (seznam.Contains(vnos))
+            {
+                txtKonzola1.Text=("Seznam vsebuje element "+vnos);
+            }
+            else
+            {
+                txtKonzola1.Text=("Seznam ne vsebuje elementa "+vnos);
+            }
+           // txtKonzola1.Clear();
+        }
+
+        private void btnLokacija_Click(object sender, EventArgs e)
+        {
+            string vnos = txtKonzola.Text;
+            int lokacija = seznam.IndexOf(vnos);
+            if (lokacija != -1)
+            {
+                txtKonzola1.AppendText("Lokacija elementa " + vnos + " v seznamu: " + lokacija);
+            }
+            else
+            {
+                txtKonzola1.AppendText("Element "+vnos+" ni bil najden v seznamu");
+            }
+           // txtKonzola1.Clear();
+        }
+
+        private void btnTrim_Click(object sender, EventArgs e)
+        {
+            seznam.TrimToSize();
+            txtKonzola1.AppendText("Seznam je skrajšan\n");
+        }
+
+        private void btnStatistika_Click(object sender, EventArgs e)
+        {
+            int velikost = seznam.Count;
+            txtKonzola1.AppendText("Velikost seznama: "+velikost);
+
+        }
+
+        private void btnIzpisi_Click(object sender, EventArgs e)
+        {
+            txtKonzola1.AppendText("Seznam:\n");
+            foreach (object element in seznam)
+            {
+                txtKonzola1.AppendText(element.ToString() + "\n");
+            }
+        } 
+        */
+    }
+}
---
+++ Vaja1/Vaja1/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Vaja1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. naloga
+            //"Znak
+            //A ima kodo 65".
+            //Program naj izpiše ustrezne stavke za znake A, B, a, b, $, + in /.
+            //char črka = 'A';
+            //int koda = črka;
+            //Console.WriteLine("Znak " + črka + " ima kodo " + koda);
+            //Console.ReadLine();
+
+            //(Del 2. naloge)
+            Console.WriteLine("5!= " + fakulteta(5));
+            //Console.ReadLine();
+
+            //(Del 3. naloge)
+            Console.WriteLine("5!= " + rekurzija(5));
+            //Console.ReadLine();
+
+            //(Del 4. naloge)
+            Console.WriteLine("2 na 10 je " + potenca2(10));
+            //Console.ReadLine();
+
+            //(Del 5. naloge)
+            Console.WriteLine("8 Fibonaccijevo število je " + Fib(8));
+            //Console.ReadLine();
+
+            //(Del 6. naloge)
+            Console.WriteLine("2. člen je " + Fib2(2) + " in 5. člen je " + Fib2(5));
+            //Console.ReadLine();
+
+            //(Del 7. naloge)
+            Console.WriteLine("2. člen je " + Fib3(2) + " in 5. člen je " + Fib3(5));
+            //Console.ReadLine();
+
+            //(Del 8. naloge)
+            Console.WriteLine("2. člen je " + Fib4(2) + " in 5. člen je " + Fib4(5));
+            Console.ReadLine();
+
+        }
+        //2. naloga
+        //Napišite program, s katerim preberete nenegativno celo število in izpišite njegovo fakulteto.
+        //Fakulteta števila n je definirana n!=n.(n-1).(n-2)….3.2.1. Primer 5!=5.4.3.2.1=120.
+        static int fakulteta(int n)
+        {
+            int produkt = 1;
+            for (int k = n; k > 0; k--)
+            {
+                produkt = produkt * k;
+            }
+            return produkt;
+        }
+        //3. naloga
+        static int rekurzija(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * rekurzija(n - 1);
+        }
+        //4. naloga
+        static int potenca2(int n)
+        {
+            if (n == 0)
+                return 1;
+            return 2 * potenca2(n - 1);
+        }
+        //5. naloga
+        //Podano je zaporedje:1,1,2,3,5,8,13,21,34, ...
+        //F1 = 1
+        //F2 = 1
+        //Fa= Fn-1 + Fn-2
+        static int Fib(int n)
+        {
+            if (n==1) return 1;
+            if (n == 2) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        /*
+        6. naloga
+        Podano je zaporedje: 2, 2, 3, 5, 14, 69, 965, ...
+a. Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+b. Napišite sled izvajanja programa za klic test.clen(2) in test.clen(5)!
+Upoštevajte, da je zaporedje definirano:
+a1 = 2
+a2 = 2
+a(n) = a(n-2)*a(n-1) – 1 
+        */
+        static int Fib2(int n)
+        {
+            if (n == 1) return 2;
+            if (n == 2) return 2;
+            return Fib2(n-2)*Fib2(n-1) - 1;
+        }
+
+        /*
+        7. naloga
+        Podano je zaporedje: 2, 8, 26, 80, 242, ...
+a. Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+b. Napišite sled izvajanja programa za klic test.clen(2) in test.clen(5).
+Upoštevajte, da je zaporedje definirano:
+a1 = 2
+a(n) = 3 * a(n-1) + 2
+        */
+        static int Fib3(int n)
+        {
+            if (n == 1) return 2;
+            if (n == 2) return 8;
+            return 3 * Fib3(n - 1) + 2;
+        }
+
+        /*
+        8. naloga
+        Podano je zaporedje:: 1, 2, 5, 12, 29, …
+a. Z uporabo rekurzije napišite metodo, ki vrne n-ti element zaporedja.
+Upoštevajte, da je zaporedje definirano:
+a1 = 1
+a2 = 2
+a(n) = a(n-1)*2 + a(n-2)
+        */
+        static int Fib4(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            return Fib4(n - 1) * 2 + Fib4(n - 2);
+        }
+    }
+}
