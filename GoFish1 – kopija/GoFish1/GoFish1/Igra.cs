1aad490bc44e71b8d3a928c5c3021467302e9b54
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-10-27 09:52:22 +0200

    Add files via upload

---
+++ GoFish1 – kopija/GoFish1/GoFish1/Igra.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFish1
+{
+    internal class Igra
+    {
+        private List<Igralec> igralci;
+        private Dictionary<Vrednosti, Igralec> kompleti;
+        private Kup talon;
+        private TextBox textNaFormi;
+        public Igra(string i, IEnumerable<string> nasprotniki, TextBox txtIgra)
+        {
+            Random r = new Random();
+            textNaFormi = txtIgra;
+            igralci = new List<Igralec>();
+            igralci.Add(new Igralec(i, r, textNaFormi));
+            foreach (string k in nasprotniki)
+                igralci.Add(new Igralec(k, r, textNaFormi));
+            kompleti = new Dictionary<Vrednosti, Igralec>();
+            talon = new Kup(); //ne pozabi, če kličem brez parametrov
+                               // imam tu vse možne karte
+            Deli();
+            igralci[0].SortRoka();
+        }
+        private void Deli()
+        {
+            //Tukaj se igra začne. Premešaj kup daj vsakemu igralcu 5 kart
+            // nato kliči metodo IzločiKomplete
+
+            talon.Mešaj();
+            for(int i = 0; i < 5; i++)
+            // 5 krat
+            {
+                //po eno vsakemu igralcu
+                foreach (Igralec ig in igralci)
+                {
+                    ig.VzemiKarto(talon.Deli());
+                }
+            }
+            foreach (Igralec ig in igralci)
+            {
+                IzločiKomplete(ig); //najprej napisana metoda
+            }
+
+        }
+        public bool IgrajEnKrog(int izbranaKarta)
+        {
+            //metoda vrača true, če je konec igre, false sicer
+            //iščemo izbrano karto, najprej igra igralec 0, nato vsi ostali
+            //takoj, ko dobimo morebitne karte od igralcev, igra preveri, če je kaj
+            // kompletov, komplete odstrani, po potrebi dodeli igralcu nove karte
+            //če zmanjka kart je igre konec
+            Vrednosti v = igralci[0].Peek(izbranaKarta).Vrednost;
+            for (int i = 1; i < igralci.Count; i++)
+            {
+                igralci[i].VprašajZaKarto(igralci, i, talon, v);
+                if (IzločiKomplete(igralci[i]))
+                    return false;
+            }
+            igralci[0].VprašajZaKarto(igralci, 0, talon, v);
+            if (IzločiKomplete(igralci[0]))
+                return false;
+            else
+                return true;
+        }
+        public bool IzločiKomplete(Igralec i)
+        {
+            //izloči komplete za posameznega igralca in vrne vrednost true
+            // če je igralec ostal brez kart
+            IEnumerable<Vrednosti> kompleti = i.IzločiKomplete();
+            foreach (Vrednosti v in kompleti)
+                kompleti.Add(v, i);
+            return i.ŠtevecKart == 0;
+        }
+        public string OpišiKomplete()
+        {
+            //vrni niz, v katerem so imena igralcev in kompleti kart
+            string rezultat = "";
+            foreach (Vrednosti v in kompleti.Keys)
+                rezultat += kompleti[v].Ime + " ima komplet " + v + Environment.NewLine;
+            return rezultat;
+        }
+        public string ImeZmagovalca()
+        {
+            //Metoda se kliče na koncu igre. Uporablja svojo strukturo - slovar, v
+            // kateri je število vseh kompletov posameznega igralca
+            //Najprej pregleda z zanko foreach število kompletov in jih doda v zbirko
+            // zmagovalcev
+
+            Dictionary<string, int> winners = new Dictionary<string, int>();
+            foreach (Vrednosti v in kompleti.Keys)
+            {
+                string ime = kompleti[v].Ime;
+                if (winners.ContainsKey(ime))
+                    winners[ime]++;
+                else
+                    winners.Add(ime, 1);
+            }
+            //Nato poišče največjo vrednost kompletov
+            int mostBooks = 0;
+            foreach (string name in winners.Keys)
+                if (winners[name] > mostBooks)
+                    mostBooks = winners[name];
+            bool tie = false;
+            string winnerList = "";
+            //določi kateri od igralcev ima to vrednost, lahko jih je več
+            foreach (string name in winners.Keys)
+                if (winners[name] == mostBooks)
+                {
+                    if (!String.IsNullOrEmpty(winnerList))
+                    {
+                        winnerList += " in ";
+                        tie = true;
+                    }
+                    winnerList += name;
+                }
+            winnerList += " z " + mostBooks + " kompleti";
+            if (tie)
+                return "Neodločeno " + winnerList;
+            else
+                return winnerList;
+        }
+        public IEnumerable<string> KarteIgralca()
+        {
+            //vrni seznam imen igralcev
+            return igralci[0].Imena();
+        }
+        public string OpišiVRokah()
+        {
+            //izpiši imena igralcev in število kart v rokah
+            // izpiši tudi koliko kart je v talonu, opis vrni kot spremenljivko tipa string
+            string opis = "Talon vsebuje " + talon.Count + " kart" + Environment.NewLine;
+            for (int i = 0; i < igralci.Count; i++)
+                opis += igralci[i].Ime + " ima " + igralci[i].ŠtevecKart + " kart" + Environment.NewLine;
+            return opis;
+        }
+
+    }
+}
---
+++ GoFish1 – kopija/GoFish1/GoFish1/Igralec.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFish1
+{
+    internal class Igralec //človek ali računalnik
+    {
+        public string Ime;
+        public Kup roka; //karte, ki jih ima igralec v roki
+        Random r=new Random();
+        TextBox textNaFormi;
+        public Igralec(string i, Random r, TextBox t)
+        {
+            //Konstruktor inicializira atribute Igralca
+            //v vnosno polje doda tekst "Joe se je pridružil igri"
+            ////in prehod 
+            //v novo vrstico
+            Ime = i;
+            this.r = r;
+            textNaFormi = t;
+            roka = new Kup(new Karta[] { });
+            textNaFormi.Text += Ime + " se je pridružil igri. \n";
+        }
+        //igralec izloči komplete
+        public IEnumerable<Vrednosti> IzločiKomplete()
+        {
+            List<Vrednosti> kompleti=new List<Vrednosti>();
+            for (int i=1; i <= 13; i++)
+            {
+                Vrednosti v = (Vrednosti)i;
+                int št = 0;
+                for (int k = 0; k< roka.Count;k++)
+                {
+                    if (roka.Peek(k).Vrednost==v)
+                    {
+                        št++;//poveča št za 1
+                    }
+                    if (št == 4)
+                    {
+                        kompleti.Add(v);
+                        roka.PullOutValues(v);
+                    }
+                }
+            }
+            return kompleti;
+        }
+        //računalnik mora izbrati naključno vrednost (od karte), a jo mora imeti v rokah
+        public Vrednosti DobiNaključnoVrednost()
+        {
+            Karta n = roka.Peek(r.Next(roka.Count));
+            return n.Vrednost;
+        }
+        //Imaš kakšno karto?
+        public Kup ImašKakšno(Vrednosti v)
+        {
+            Kup imam = roka.PullOutValues(v);
+            textNaFormi.Text += Ime + " ima " + imam.Count + " " + v +Environment.NewLine;
+            return imam;
+        }
+        public void VprašajZaKarto(List<Igralec> i,int mojIndeks, Kup talon, Vrednosti v)
+        {
+            //i=seznam vseh igralcev, mojIndeks= sem jaz
+            textNaFormi.Text += Ime + " sprašujem, če ima kdo " + v + Environment.NewLine;
+            int daneKarteSkupaj = 0;
+            //vprašaj vse igralce
+            for(int k=0; k<i.Count;k++)
+            {
+                //razen mene
+                if (k != mojIndeks)
+                {
+                    Igralec ig = i[k];
+                    Kup daneKarte = ig.ImašKakšno(v);
+                    daneKarteSkupaj += daneKarte.Count;
+                    while(daneKarte.Count > 0)
+                    {
+                        roka.Add(daneKarte.Deli());
+                    }
+                }
+            }//konec pregleda vseh kart drugih igralcev
+            if (daneKarteSkupaj==0 && talon.Count > 0)
+            {
+                textNaFormi.Text += Ime + " mora potegniti karto iz talona." + Environment.NewLine;
+                roka.Add(talon.Deli());
+            }
+        }
+        public void VprašajZaKarto(List<Igralec>i,int mojIndeks, Kup talon)
+        {
+            if (talon.Count > 0)
+            {
+                if (roka.Count > 0)
+                {
+                    roka.Add(talon.Deli());
+                }
+                Vrednosti n = DobiNaključnoVrednost();
+                VprašajZaKarto(i, mojIndeks, talon, n);
+            }
+        }
+        public int ŠtevecKart { get { return roka.Count; } }
+        public void VzemiKarto(Karta k)
+        {
+            roka.Add(k);
+        }
+        public IEnumerable<string> Imena()
+        {
+            return roka.ImenaKart();
+        }
+        public Karta Peek(int št)
+        {
+            return roka.Peek(št);
+        }
+        public void SortRoka()
+        {
+            roka.Sort();
+        }
+    }
+}
