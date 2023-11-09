0a8dc326d963ad8e23a8f9509d091d399c3f545e
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-09 10:51:15 +0100

    Add files via upload

---
+++ Standardi/Standardi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Standardi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("liga.xml", FileMode.Create);
+            XmlSerializer s = new XmlSerializer(typeof(NogometnaLiga));
+            NogometnaLiga n=new NogometnaLiga();
+            s.Serialize(fs, n);
+            fs.Close();
+            Console.WriteLine("Konec");
+            Console.ReadLine();
+        }
+    }
+}
