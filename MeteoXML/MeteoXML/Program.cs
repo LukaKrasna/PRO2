45ed5bb8ffb1d58484d0bbe1e954764b73352d3d
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-10 09:21:43 +0100

    Add files via upload

---
+++ MeteoXML/MeteoXML/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MeteoXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("D:\\Pro2\\MeteoXML\\MeteoXML\\Meteo.xml", FileMode.Open);
+            XmlSerializer s=new XmlSerializer(typeof(data));
+            data podatki=(data)s.Deserialize(fs);
+            foreach(dataMetData x in podatki.metData) 
+            {
+                Console.WriteLine(x.validStart+" "+ x.tavg+" "+x.valid_UTC);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ vajaXMLdeserializacija/vajaXMLdeserializacija/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace vajaXMLdeserializacija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new
+            FileStream(@"D:\Pro2\vajaXMLdeserializacija\vajaXMLdeserializacija\Vaja.xml", FileMode.Open);
+            XmlSerializer s = new XmlSerializer(typeof(IzdaniRacunEnostavni));
+            IzdaniRacunEnostavni a = (IzdaniRacunEnostavni)s.Deserialize(fs);
+            foreach (var x in a.Racun.PostavkeRacuna)
+            {
+                Console.WriteLine(x.OpisiArtiklov.OpisArtikla.OpisArtikla1 + " " + x.KolicinaArtikla.Kolicina + " " + x.ZneskiPostavke.ZnesekPostavke);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
