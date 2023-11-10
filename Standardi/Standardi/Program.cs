a0d87fcd20607fee4be2418e804593218ce224df
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-10 09:44:24 +0100

    Add files via upload

--- Standardi/Standardi/Program.cs
+++ Standardi/Standardi/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,15 +19,33 @@ namespace Standardi
             //NogometnaLiga n=new NogometnaLiga();
             //s.Serialize(fs, n);
             //fs.Close();
-            FileStream fs = new FileStream("d:\\pro2\\liga.xml", FileMode.Open);
-            XmlSerializer s = new XmlSerializer(typeof(NogometnaLiga));
-            NogometnaLiga x = null;
-            x=(NogometnaLiga)s.Deserialize(fs);
-            foreach(Ekipa a in x.liga)
+
+            //FileStream fs = new FileStream("d:\\pro2\\liga.xml", FileMode.Open);
+            //XmlSerializer s = new XmlSerializer(typeof(NogometnaLiga));
+            //NogometnaLiga x = null;
+            //x=(NogometnaLiga)s.Deserialize(fs);
+            //foreach(Ekipa a in x.liga)
+            //{
+            //    Console.WriteLine(  a.Ime);
+            //}
+            //Console.WriteLine("Konec");
+
+            //FileStream fs = new FileStream("liga.json", FileMode.Create);
+            //NogometnaLiga n = new NogometnaLiga();
+            //string zaPisanje=JsonConvert.SerializeObject(n,Formatting.Indented);
+            //StreamWriter sw=new StreamWriter(fs);
+            //sw.Write(zaPisanje);
+            //sw.Close();
+            //Console.WriteLine("Konec");
+
+            FileStream fs = new FileStream("liga.json", FileMode.Open);
+            StreamReader sr=new StreamReader(fs);
+            string prebrano =sr.ReadToEnd();
+            NogometnaLiga n = JsonConvert.DeserializeObject<NogometnaLiga>(prebrano);
+            foreach (Ekipa x in n.liga)
             {
-                Console.WriteLine(  a.Ime);
+                Console.WriteLine(x.Ime);
             }
-            Console.WriteLine("Konec");
             Console.ReadLine();
         }
     }
