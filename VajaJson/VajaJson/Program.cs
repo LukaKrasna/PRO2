a8814f312cc1278902f5a1c3f3c415c57093cf7e
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-10 10:00:18 +0100

    Add files via upload

---
+++ VajaJson/VajaJson/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VajaJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("Podatki.json", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string prebrano = sr.ReadToEnd();
+            List<Poglavje> n = JsonConvert.DeserializeObject<List<Poglavje>>(prebrano);
+            foreach ( x in n.)
+            {
+                Console.WriteLine(x.Ime);
+            }
+            Console.ReadLine();
+        }
+    }
+}
