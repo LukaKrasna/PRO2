71e585a9a41e6293d8170fafbdb05bdcbd4c0627
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-24 09:54:10 +0100

    Add files via upload

---
+++ LinqPoizvedbe/LinqPoizvedbe/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPoizvedbe
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //var prva = 7;
+            //Console.WriteLine(prva.GetType());
+            //var druga = "Pozdravljen svet";
+            //Console.WriteLine(druga.GetType());
+            //var tretja = druga;
+            //Console.WriteLine(tretja.GetType());
+
+            //omejitve: ne sme biti izvodova spremenljivka
+            //ne sme biti izvodova spremenjivka
+            //ne sme biti tip vrnjene vrednosti
+            //ne smemo ji prirediti null
+
+            //string[] države = { "USA", "Canada", "UK", "Australia", "Germany", "Sweden", "Mexico", "France", "Italy", "Uganda", "Ukraine", "Spain" };
+            ////brez linq
+            //for (int k=0; k < države.Length; k++)
+            //{
+            //    if (države[k].StartsWith("U"))
+            //        Console.WriteLine(države[k]);
+            //}
+            ////z linq
+            //var uDržave = from d in države
+            //              where d.StartsWith("U")
+            //              select d;
+            //Console.WriteLine("*******************************");
+            //foreach(var x in uDržave)
+            //    Console.WriteLine(x);
+
+            ////izpiši države, ki imajo ime daljše od 5
+            //var xDržave = from d in države
+            //              where d.Length >= 5
+            //              select d;
+            //Console.WriteLine("*******************************");
+            //foreach (var x in xDržave)
+            //    Console.WriteLine(x);
+
+            ////vse države, ki se začnejo z U in ime je daljše od 5
+
+            //var yDržave = from d in države
+            //              where d.StartsWith("U") && d.Length >= 5
+            //              select d;
+            //Console.WriteLine("*******************************");
+            //foreach (var x in yDržave)
+            //    Console.WriteLine(x);
+            //var urejeneDržave = from d in države
+            //                    orderby d
+            //                    select d;
+            //Console.WriteLine("*******************************");
+            //foreach (var x in urejeneDržave)
+            //    Console.WriteLine(x);
+            //Console.ReadLine();
+
+
+            //tu je glavni program
+            List<Naročilo> naročilaList = setupNaročila();
+            //izberi vse podatke o naročilih
+            var x1 = from a in naročilaList
+                     select a;
+            Console.WriteLine("**********************************");
+            foreach (var x in x1 )
+                Console.WriteLine(x.NaročiloID + " " + x.Datum.ToString());
+            //v rezultatu bi želel samo datume
+            var x2 = from a in naročilaList
+                     select a.Datum;
+            Console.WriteLine("**********************************");
+            foreach (var x in x2)
+                Console.WriteLine(x.ToShortDateString());
+
+            //v rezultatu hočem samo id naročila in datum
+            var x3 = from a in naročilaList
+                     select new {a.NaročiloID, a.Datum};
+            Console.WriteLine("**********************************");
+            foreach (var x in x3)
+                Console.WriteLine(x.NaročiloID + " " + x.Datum.ToShortDateString());
+            
+            //podrobnosti naročila
+            var x4= from a in naročilaList
+                    from b in a.Elementi
+                    select b;
+            Console.WriteLine("**********************************");
+            foreach (var x in x4)
+                Console.WriteLine(x.ImeIzdelka + " " + x.Količina);
+
+            //izpiši naročilo 9010
+            var x5 = from a in naročilaList
+                     where a.NaročiloID == 9010
+                     select new { a.Datum };
+            Console.WriteLine("**********************************");
+            foreach (var x in x5)
+                Console.WriteLine(x.Datum.ToShortDateString());
+
+            var x6 = (from a in naročilaList
+                     where a.NaročiloID == 9010
+                     select new { a.Datum }).FirstOrDefault();
+            Console.WriteLine("Datum je "+x6.Datum.ToShortDateString());
+
+            //izpiši naročila, ki imajo več kot 2 vrstici
+            var x7 = from a in naročilaList
+                     where a.Elementi.Count > 2
+                     select a;
+            Console.WriteLine("**********************************");
+            foreach (var x in x7)
+                Console.WriteLine(x.NaročiloID + " " + x.Datum.ToShortDateString());
+
+            int[] števila = { 46, 24, 79, 35, 12, 57, 80, 68 };
+            var r1 = števila.Count();
+            Console.WriteLine("\t" + r1);
+            var r2 = števila.Sum();
+            Console.WriteLine("\t"+r2);
+            var r3 = števila.Min();
+            Console.WriteLine("\t"+r3);
+            var r4 = števila.First();
+            Console.WriteLine("\t"+r4);
+            var r5 = števila.Distinct().Count();
+            Console.WriteLine("\t" + r5);
+            Console.ReadLine();
+        }
+        static private List<Naročilo> setupNaročila()
+        {
+            List<Naročilo> NaročiloList = new List<Naročilo>();
+            Naročilo o1 = new Naročilo();
+            o1.Datum = DateTime.Parse("12.7.2010");
+            o1.NaročiloID = 9009;
+            PodrobnostiNaročila oi1 = new PodrobnostiNaročila();
+            oi1.ElementID = 123;
+            oi1.ImeIzdelka = "Mars";
+            oi1.Količina = 2;
+            o1.Elementi.Add(oi1);
+            PodrobnostiNaročila oi2 = new PodrobnostiNaročila();
+            oi2.ElementID = 124;
+            oi2.ImeIzdelka = "Kraš";
+            oi2.Količina = 3;
+            o1.Elementi.Add(oi2);
+            Naročilo o2 = new Naročilo();
+            o2.Datum = DateTime.Parse("12.1.2011");
+            o2.NaročiloID = 9010;
+            PodrobnostiNaročila oi3 = new PodrobnostiNaročila();
+            oi3.ElementID = 125;
+            oi3.ImeIzdelka = "Mars";
+            oi3.Količina = 1;
+            o2.Elementi.Add(oi3);
+            PodrobnostiNaročila oi4 = new PodrobnostiNaročila();
+            oi4.ElementID = 126;
+            oi4.ImeIzdelka = "Extreme";
+            oi4.Količina = 5;
+            o2.Elementi.Add(oi4);
+            PodrobnostiNaročila oi5 = new PodrobnostiNaročila();
+            oi5.ElementID = 127;
+            oi5.ImeIzdelka = "Bazooka";
+            oi5.Količina = 1;
+            o2.Elementi.Add(oi5);
+            PodrobnostiNaročila oi6 = new PodrobnostiNaročila();
+            oi6.ElementID = 128;
+            oi6.ImeIzdelka = "Sadje";
+            oi6.Količina = 6;
+            o2.Elementi.Add(oi6);
+            NaročiloList.Add(o1);
+            NaročiloList.Add(o2);
+
+            return NaročiloList;
+        }
+    }
+}
---
+++ vajeLinq1/vajeLinq1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vajeLinq1
+{
+    internal class Program
+    {
+        //naloga: https://ucilnice.scng.si/pluginfile.php/31856/mod_resource/content/0/LINQObjectsVaje.pdf
+        static void Main(string[] args)
+        {
+            var kupci = new[]{
+            new {KupecID=1,Ime="Janez",Priimek="Kranjski",Podjetje="Kolo"},
+            new {KupecID=2,Ime="Miha",Priimek="Polanc",Podjetje="Djak"},
+            new {KupecID=3,Ime="Gašper",Priimek="Rupnik",Podjetje="Fitnes"},
+            new {KupecID=4,Ime="Martin",Priimek="Bolčina",Podjetje="Kolo"},
+            new {KupecID=5,Ime="Alenka",Priimek="Puncer",Podjetje="Kolo"},
+            new {KupecID=6,Ime="Mojca",Priimek="Širok",Podjetje="Djak"},
+            new {KupecID=7,Ime="Peter",Priimek="Gulin",Podjetje="Djak"},
+            new {KupecID=8,Ime="Pavel",Priimek="Gantar",Podjetje="Inn"},
+            new {KupecID=9,Ime="David",Priimek="Niven",Podjetje="Inn"},
+            new {KupecID=10,Ime="Erik",Priimek="Kompara",Podjetje="Fitnes"}
+            };
+            var podjetja = new[] {
+            new {ImePodjetja="Kolo",Mesto="Nova Gorica",Država="Slovenija"},
+            new {ImePodjetja="Djak",Mesto="Nova Gorica",Država="Slovenija"},
+            new {ImePodjetja="Fitnes",Mesto="Ljubljana",Država="Slovenija"},
+            new {ImePodjetja="Inn",Mesto="Trst",Država="Italija"},
+            };
+            //1. napiši LINQ stavek s katerim izbereš in izpišeš vsa imena kupcev
+            var a = from ab in kupci
+                    select ab.Ime;
+            foreach (var ac in a)
+                Console.WriteLine(ac.ToString());
+            Console.WriteLine("****************************");
+            //2. napiši LINQ stavek katerim izbereš in izpišeš imena in priimke kupcev
+            var b = from bb in kupci
+                    select new { bb.Ime, bb.Priimek};
+            foreach (var bc in b)
+                Console.WriteLine(bc.Ime.ToString() + " " + bc.Priimek.ToString());
+            Console.WriteLine("****************************");
+            //3. izberi in izpiši vsa imena podjetji iz Slovenije
+            var c = from cb in podjetja
+                    select cb.ImePodjetja;
+            foreach (var cc in c)
+                Console.WriteLine(cc.ToString());
+            Console.WriteLine("****************************");
+            //4. Izpiši imena podjetji urejena po abecedi
+            var d = from db in podjetja
+                    orderby db.ImePodjetja
+                    select db;
+            foreach (var dc in d)
+                Console.WriteLine(dc.ImePodjetja);
+            Console.WriteLine("****************************");
+            //5. izpiši koliko je različnih podjetji
+            var e = (from eb in podjetja
+                      select eb).Count();
+            Console.WriteLine("Število podjetji je " + e.ToString() + ".");
+            Console.WriteLine("****************************");
+            //6. izpiši koliko podjetij je iz Italije
+            var f = (from fb in podjetja
+                      where fb.Država == "Italija"
+                      select fb).Count();
+            Console.WriteLine("Število podjetji je " + f.ToString() + ".");
+            Console.WriteLine("****************************");
+            //7. izpiši iz koliko različnih držav imamo podjetja
+            var g = (from gb in podjetja
+                     select gb.Država).Distinct().Count();
+            Console.WriteLine("Podjetja imamo iz " + g.ToString() + " različnih držav.");
+            Console.WriteLine("****************************");
+            Console.ReadLine();
+        }
+    }
+}
