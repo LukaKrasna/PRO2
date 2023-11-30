c62535b87e7f7fd88ffe2205a32519464e1e76b3
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-11-30 10:58:07 +0100

    Add files via upload

---
+++ LINQtoENTITIES/LINQtoENTITIES/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoENTITIES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //naloga 7.ma : https://ucilnice.scng.si/pluginfile.php/32040/mod_resource/content/0/LINQ_to_Entitites.pdf
+            nwEntities1 nw = new nwEntities1();
+            nw.Database.Log = Console.WriteLine;
+            var pijače = from p in nw.Products
+                         where p.Categories.CategoryName == "Beverages"
+                         orderby p.ProductName
+                         select p;
+            Console.WriteLine("Imamo "+pijače.Count()+ " pijač.");
+            var pi=nw.Products.Where(a=>a.Categories.CategoryName == "Beverages").OrderBy(e=>e.ProductName).Count();
+            Console.WriteLine("Imamo "+pi+"pijač.");
+            Products produkt=pijače.FirstOrDefault();
+            Console.WriteLine("stara cena " +produkt.UnitPrice);
+            if (produkt != null)
+            {
+                decimal novaCena = (decimal)produkt.UnitPrice + 10;
+                produkt.UnitPrice = novaCena;
+                nw.SaveChanges();
+            }
+            Console.WriteLine("Nova cena "+produkt.UnitPrice);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UvodVDelegate/UvodVDelegate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UvodVDelegate
+{
+    internal class Program
+    {
+        //1. definiraj delegata - ni potreben
+        //public delegate bool FunkcijaZaNize(string s);
+        //2. uporabi delegata kot parameter metide
+        public static List<string> DelajOperacijeNadNizi(string[] nizi,Func<string,bool> funkcija)
+        {
+            List<string> rezultat = new List<string>();
+            foreach (string s in nizi)
+            {
+                if (funkcija(s))
+                    rezultat.Add(s);
+            }
+            return rezultat;
+        }
+        //3. napiši metode, ki so tipa FunkcijeZaNize
+        //public static bool ZačneZA(string s)
+        //{
+        //    return s.StartsWith("A");
+        //}
+        public static bool KončaZN(string s)
+        {
+            return s.EndsWith("n");
+        }
+        static void Main(string[] args)
+        {
+            //string[] mojiNizi = { "Adam", "Aida", "Bob", "Steve", "Jim", "Alan", "Rob", "Bill", "Jackob", "James" };
+            ////FunkcijaZaNize anonimna = delegate (string s) { return s.StartsWith("A"); };
+            ////List<string> x = DelajOperacijeNadNizi(mojiNizi, anonimna);
+            //List<string> x=DelajOperacijeNadNizi(mojiNizi, s=>s.StartsWith("A"));
+            //foreach(string s in x)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //List<string> y = DelajOperacijeNadNizi(mojiNizi, KončaZN);
+            //foreach (string s in y)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+
+            int[] števila = { 5, 10, 8, 3, 6, 12 };
+            var r1 = from a in števila
+                     where a % 2 == 0
+                     orderby a
+                     select a;
+            foreach(var x in r1)
+            {
+                Console.WriteLine(x);
+            }
+            var r2 = števila.Where(a => a % 2 == 0).OrderBy(a => a);
+            foreach (var x in r2)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
