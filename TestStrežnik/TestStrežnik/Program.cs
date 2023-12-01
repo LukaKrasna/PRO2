f7f4c5d81e74fb4a6d383fae98d2d06877203341
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-12-01 10:11:03 +0100

    Add files via upload

---
+++ TestStrežnik/TestStrežnik/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStrežnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorksLT2019Entities dc=new AdventureWorksLT2019Entities();
+            var r = dc.Product.Select(a => a);
+            foreach(var r2 in r)
+            {
+                Console.WriteLine(r2.Name + " " + r2.ProductNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VajaDelegati/VajaDelegati/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaDelegati
+{
+    internal class Program
+    {
+        delegate string Spremeni(string s);
+        static void Main(string[] args)
+        {
+            Func<string,string> mojaMetoda = s=>s.ToLower();
+            string ime = "Dakota";
+            Console.WriteLine(mojaMetoda(ime));
+            Console.ReadLine();
+        }
+        //static string WeLike(string s)
+        //{
+        //    return s.ToUpper();
+        //}
+        //static string VMale(string s)
+        //{
+        //    return s.ToLower();
+        //}
+    }
+}
