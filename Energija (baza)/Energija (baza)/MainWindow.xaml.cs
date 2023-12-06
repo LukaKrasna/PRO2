38d3441a5cf1888fad0037370aaef3369392f20d
LukaKrasna <122783234+LukaKrasna@users.noreply.github.com>
2023-12-06 09:19:53 +0100

    Add files via upload

---
+++ Energija (baza)/Energija (baza)/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Energija__baza_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ElektrikaEntities en = new ElektrikaEntities();
+            var x = (from a in en.Meritve
+                    where a.ZapisČas.Value.Day == 18 && a.ZapisČas.Value.Month == 8 && a.ZapisČas.Value.Year == 2013
+                    group a by a.ZapisČas.Value.Hour into z
+                    select new 
+                    {
+                       Ura=z.Key,
+                       Moč=z.Average(b=>b.kW1+b.kW2+b.kW3)
+                    }).ToList();
+                    ;
+            //string rezultat = "";
+            //foreach (var y in x)
+            //{
+            //    rezultat += y.Ura +"  "+ y.Moč+ "\n";
+            //}
+            //MessageBox.Show(rezultat);
+            CollectionViewSource cvs = (CollectionViewSource)this.FindResource("cvs");
+            cvs.Source = x;
+        }
+    }
+}
---
+++ Energija (baza)/VajaLINQtoENTITIES3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaLINQtoENTITIES3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //naloga:https://ucilnice.scng.si/pluginfile.php/41749/mod_resource/content/0/VajaLinqToEntities.pdf
+            //1. izberi čas meritve in skupno moč
+            //ista poizvedba z lambda izrazom
+
+            //2. izberi čas meritve in skupno moč za datum 18.8.2013
+            //3. izračunaj povprečno moč za datum 18.8.2013
+            //4. izračunaj maximalno moč za ta datum
+            //5. izračunaj minimalno moč za ta datum
+            //6. izračunaj povprečno moč po urah za dan 18.8.2013
+            //7. izračunaj 15 minutna povprečja za 18.8.2013
+        }
+    }
+}
